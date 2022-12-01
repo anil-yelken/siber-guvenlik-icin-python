172313e7a687218712b77b7e540d344f524972db
codingkunal <119115502+codingkunal@users.noreply.github.com>
2022-12-01 11:51:16 -0800

    Update nmap_1.py

--- nmap_1.py
+++ nmap_1.py
@@ -1,6 +1,6 @@
 import nmap
-#python-nmap
-nmap = nmap.PortScanner()
+#python-nmap 
+nmap = nmap.PortScanner() # nmap is the program of python port scanner....
 nmap.scan('127.0.0.1', '1-65000','-sV')
 print "Nmap komutu: ",nmap.command_line()
 print "Tarama bilgisi: ",nmap.scaninfo()
